75393c6f8052144a9cacd0878070aabd1e630dfc
simona <palumbosimona118@gmail.com>
2020-02-20 17:06:03 +0100

    esercizio ok!

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
+<!--Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relative lunghezza e
+sostituire la badword passata in GET con tre *. (edited) -->
+
 
 <?php
 $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 //var_dump = $testo;
-$change_word = $_GET["cambio"];
+$change_word = $_GET["parola"];
 var_dump($_GET);
-
  ?>
-
-
-
-
 
 
 <!DOCTYPE html>
